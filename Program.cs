6968533d53755618fdba81586fc72fbc14643272
Diego Viana <diego.viana@tungsten-network.com>
2018-04-09 16:57:42 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
